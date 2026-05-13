32793835aa2c019e98bc0e349896613f80bb7396
ronvree <ronvbree@gmail.com>
2026-05-13 13:46:32 +0200

    evaluation __init__ comparison update

--- src/pysephone/evaluation/__init__.py
+++ src/pysephone/evaluation/__init__.py
@@ -1,1 +1,14 @@
 from pysephone.evaluation.regression import EvaluationException, SingleTargetRegression
+from pysephone.evaluation.model_comparison import (
+    ComparisonReport,
+    EvaluationRun,
+    MissingPolicy,
+    autorank_report,
+    build_scores_table,
+    compare_models,
+    friedman_nemenyi,
+    plot_critical_difference,
+    plot_nemenyi_heatmap,
+    plot_rank_distribution,
+    plot_score_heatmap,
+)
